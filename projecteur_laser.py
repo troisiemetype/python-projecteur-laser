0dd7f931f669349ac06ed9ac31c6d1987cee4103
Pierre-Loup Martin <pierreloupm@gmail.com>
2016-04-14 10:05:14 +0200

    Added a function that transforms the opne image into a bixbuf Displays the image once open

--- projecteur_laser.py
+++ projecteur_laser.py
@@ -12,6 +12,9 @@ import serial.tools.list_ports
 
 from PIL import Image
 
+import array
+from gi.repository import GdkPixbuf
+
 im = Image.new('RGB', (20,20))
 #thumbnail = Image
 
@@ -246,16 +249,41 @@ def serialGetPorts():
 
 #This function deals with openning a new file
 def openFile(uri):
+    #try to open the file, else give an Error message
     try:
         im = Image.open(uri)
     except IOError:
         wm.messageErreur('Veuillez choisir un fichier image',
                          'Fichiers pris en charge: bmp, jpg, png, tiff, svg')
         return False
+    #display status
     wm.status('open %s' %uri)
+    #Load the image, get its size, create thumbnail
     im.load()
-    #wm.image.set_from_file(uri)
+    w,h = im.size
+    thumb = im
+    thumb.thumbnail((450, int(450*h/w)))
+    #sets the thumbnail in the image area, through the imageToPixbuf function
+    wm.image.set_from_pixbuf(imageToPixbuf(thumb))
     return True
+
+#this function converts PIL images to Pixbuf format for displaying in Gtk
+def imageToPixbuf(im):
+    #transforms the given image into an array of pixels
+    arr = array.array('B', im.tostring())
+    w,h=im.size
+    #look at a an alpha mask
+    if im.mode == 'RGBA':
+        hasAlpha = True
+        dist = w*4
+    else:
+        hasAlpha = False
+        dist = w*3
+    #returns the pix buf. Args:
+    #array, colorspace, has alpha, bits per sample,
+    #width, height, distance in bytes between row starts
+    return GdkPixbuf.Pixbuf.new_from_data(arr, GdkPixbuf.Colorspace.RGB, hasAlpha, 8, w, h, dist)
+
     
 
 #instanciates the config and window objects.
