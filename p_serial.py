9c48140af8de515a705d3b3bc0c5665a8cb8d90f
Pierre-Loup Martin <pierreloupm@gmail.com>
2016-04-25 16:10:27 +0200

    Modified: serial functions, send, read and parse.

--- p_serial.py
+++ p_serial.py
@@ -10,6 +10,8 @@
 
 import serial
 from serial.tools import list_ports
+from time import sleep, time
+
 
 class SerialLink(serial.Serial):
     '''Handle the serial object. Define methods for sending data from image data buffer.'''
@@ -34,9 +36,12 @@ class SerialLink(serial.Serial):
         self.send_ok_flag = 1
         #set when the projector asks again for the last line.
         self.send_again_flag = 0
+        #keeps track of the data type received.
+        self.data_type = 'json'
         #counter index used in loops.
         self.i = 0
-    
+       
+       
     #init the instance of the class with values comming from the config file
     #values are passed trough a dictionnary
     def init_from_cfg(self, cfg):
@@ -62,7 +67,7 @@ class SerialLink(serial.Serial):
     #This returns a list of the baudrates available (for the settings menu construction)
     def get_baudrates(self):
         """Create a list containing available baudrates."""
-        #TODO: try greater speed: 230400, 460800, 500000.
+        #TODO: try greater speed: 230400, 250000, 460800, 500000.
         tupl_baud = (300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200)
         return tupl_baud
     
@@ -90,8 +95,6 @@ class SerialLink(serial.Serial):
         #exit calibration, close port and display a message.
         try:
             self.write(string_to_send)
-            SerialLink.wm.debug_append('>>> ' + string_to_send)
-            self.send_ok_flag = 0
         except serial.SerialException:
             SerialLink.wm.toolbutton_calibrate.set_active(0)
             SerialLink.wm.message_erreur('Le port a été déconnecté',
@@ -100,6 +103,10 @@ class SerialLink(serial.Serial):
             self.i = 0
             self.close()
             return 1
+        else:
+            SerialLink.wm.debug_append('>>> ' + string_to_send)
+            self.send_ok_flag = 0   
+            
         
         #Only if we are in a normal iteration (i.e. not stopping), increment self.i
         if self.calibrate_flag == 1:
@@ -111,86 +118,57 @@ class SerialLink(serial.Serial):
         return 1
     
     #This function sends the data to the board when the flag is on
-    def send_data(self):
+    def send_data(self,):
         """Send data to projector.
         Verifies flags states.
         Write string to buffer, update GUI.
-        Handle serial esceptions.
+        Handle serial exceptions.
         Re-init flags state when finished.
         """
-        #Don't do anything if the send flag is not set.
+        #Look for wanted state.
         if self.send_flag == 0:
             return
-        #If the pause falg is set, don't do neither
         elif self.pause_flag == 1:
             return
-        #If stop flag == 1, then stop.
+        #If stop flag, re-init for next send.
         elif self.stop_flag == 1:
+            self.i = 0
             self.send_flag = 0
             self.stop_flag = 0
             self.send_ok_flag = 1
-            self.i = 0
-            SerialLink.wm.set_gui_group('send', True)
-            SerialLink.wm.progress_total.hide()
-            SerialLink.wm.message_info("Exposition interrompue par l'utilisateur")
+            SerialLink.wm.set_gui_group('send', True);
+            SerialLink.wm.progress_total.hide();
             return
-            
-        #If the board hasn't asked for datas
+        
+        #If no data is asked by projector, return.
         if self.send_ok_flag == 0:
             return
-
-        #If their was a problem with the last string sent, send it again.
-        if self.send_again_flag == 1:
-            self.i -= 1
-            self.send_again_flag = 0
-            
-            
-        #Some things to do when sending begins.
-        if self.i == 0:
-            SerialLink.wm.set_gui_group('send', False)
-            SerialLink.wm.progress_total.show()
-
-            
-        string_to_send = SerialLink.im.data_buffer[self.i]
-#        print(string_to_send);
-      
-#        SerialLink.wm.debug_append('>>> ' + string_to_send)
         
+        #First iteration: some things to set.
+        if self.i == 0:
+            self.start_time = time()
+            SerialLink.wm.set_gui_group('send', False);
+            SerialLink.wm.progress_total.show();
+        
+        string_to_send = SerialLink.im.data_buffer[self.i]
+        #self.flush()
+        self.write(string_to_send.encode('utf-8'))
+        #print(string_to_send)
+        
+        self.send_ok_flag = 0
         self.i += 1
         
-        try:
-            sent_bytes = self.write(string_to_send.encode('utf-8'))
-#            SerialLink.wm.debug_append('>>> ' + string_to_send)
-            print(sent_bytes)
-            self.send_ok_flag = 0
-        except serial.SerialException as e:
-            SerialLink.wm.message_erreur('Le port a été déconnecté',
-                                   'Vérifiez la connexion au projecteur')
-            print(e)
-            self.data_flag = 0
+        if self.i >= SerialLink.im.pix_qty:
+            sending_time = time()-self.start_time
             self.i = 0
-            self.close()
-            return 1
-        except serial.SerialTimeoutException as e:
-            print(e)
-            return 1
-
-        #Update the sending process at each new line
-        if self.i % (SerialLink.im.width +2):
-            SerialLink.wm.progress_total.set_fraction(self.i / SerialLink.im.pix_qty)
-
-        #Stop sending if we are at end of data_list.
-        if self.i >= len(SerialLink.im.data_buffer):
             self.send_flag = 0
-            self.i = 0
-            SerialLink.wm.message_info("Envoi terminé")
+            self.send_ok_flag = 1
+            SerialLink.wm.set_gui_group('send', True);
+            SerialLink.wm.progress_total.hide();
+            SerialLink.wm.message_info("Image insolée en %.3fs."%sending_time)
             
-        #If self.send_flag == 0, sending has ended:
-        #Hide progres bar and re-enable toolbuttons
-        if self.send_flag == 0:
-            self.i = 0
-            SerialLink.wm.set_gui_group('send', True)
-            SerialLink.wm.progress_total.hide()
+            
+
     
     def send_cfg(self, var, value):
         """Send a configuration value to the board."""
@@ -207,17 +185,22 @@ class SerialLink(serial.Serial):
         """
         if not self.is_open:
             return
-        if self.in_waiting == 0:
+        nb_byte = self.in_waiting
+        if nb_byte == 0:
             return
         
-        raw_byte = self.read()
-        print(raw_byte)
-        
-        if raw_byte != b'\n':
-            self.raw_data += raw_byte.decode('utf-8', errors='ignore')
-        else:
-            self.data_flag = 1
-
+        for i in range(nb_byte):
+            raw_byte = self.read()
+            if raw_byte == b'$':
+                self.data_type = 'cfg'
+            if raw_byte == b'{':
+                self.data_type = 'json'
+                
+            if raw_byte == b'\n':
+                self.data_flag = 1
+                return
+            else:
+                self.raw_data += raw_byte.decode('utf-8', errors='ignore')
 
     def parse_data(self):
         """Parse data received.
@@ -229,36 +212,44 @@ class SerialLink(serial.Serial):
         else:
             self.data_flag = 0
         
-        #send raw data and get values pairs from the json parser.
-        data = SerialLink.jsp.from_json(self.raw_data)
-        self.raw_data = ""
-        print(data)
-        
-        #Stop working if the data is not well formated.
-        if type(data) is not dict:
-            SerialLink.wm.status('Erreur de valeur')
-            return
-        
-        #Look if there is a send flag.
-        send_flag = data.get('send')
-        if send_flag == 1:
-            self.send_ok_flag = 1
-        #elif send_flag == 2:
-            #self.send_again_flag = 1
-            #self.send_ok_flag = 1
-            #SerialLink.wm.status('Coordonnée envoyée à nouveau')
+        if self.data_type == 'json':
+            #send raw data to and get values pairs from the json parser.
+            data = SerialLink.jsp.from_json(self.raw_data)
+            self.raw_data = ""
+            print(data)
             
-        #progress = data.get('progress')
-        #if progress != None:
-            #percent = 100 * data.get('ID')/SerialLink.im.pix_qty
-            #SerialLink.wm.progress_total.set_text('Insolation image: %2d%%'%(percent))
-            #SerialLink.wm.progress_total.set_fraction(data.get('ID')\
-                                                       #/SerialLink.im.pix_qty)
-
-        message = data.get('message')
-        if message != None:
-            SerialLink.wm.status(message)
+            #Stop working if the data is not well formated.
+            if type(data) is not dict:
+                SerialLink.wm.status('Erreur de valeur')
+                return
             
-        #erreur = data.get('erreur')
-        #if erreur != None:
-            #print(erreur)
+            #Look if there is a send flag.
+            send_flag = data.get('send')
+            if send_flag == 1:
+                self.send_ok_flag = 1
+            #elif send_flag == 2:
+                #self.send_again_flag = 1
+                #self.send_ok_flag = 1
+                #SerialLink.wm.status('Coordonnée envoyée à nouveau')
+                
+            #progress = data.get('per')
+            #if progress != None:
+                #percent = progress/SerialLink.im.pix_qty
+                #SerialLink.wm.progress_total.set_text('Insolation image: %2d%%'%(percent))
+                #SerialLink.wm.progress_total.set_fraction(percent)
+    
+            message = data.get('message')
+            if message != None:
+                SerialLink.wm.status(message)
+                
+            #erreur = data.get('erreur')
+            #if erreur != None:
+                #print(erreur)
+        else:
+            print(self.raw_data)
+            data = self.raw_data.lstrip('$')
+            self.raw_data = ''
+            if data == 's':
+                self.send_ok_flag = 1
+                
+            
